47e5f1826180958fa62d656d75d9430978aa0f66
Eric Richards <ericrrichards@gmail.com>
2016-03-06 12:08:45 -0500

    add some comments to the Vector3 class

--- OneWeekendRT/Util/Vector3.cs
+++ OneWeekendRT/Util/Vector3.cs
@@ -2,12 +2,17 @@ namespace OneWeekendRT.Util {
     using System;
 
     public struct Vector3 {
+        // XYZ fields
         public float X;
         public float Y;
         public float Z;
+
+        // RGB aliases
         public float R { get { return X; } set { X = value; } }
         public float G { get { return Y; } set { Y = value; } }
         public float B { get { return Z; } set { Z = value; } }
+
+        // Indexer for treating the vector as an array
         public float this[int i] {
             get {
                 switch (i) {
@@ -38,27 +43,32 @@ namespace OneWeekendRT.Util {
             }
         }
 
+        // Simple constructor
         public Vector3(float x, float y, float z) {
             X = x;
             Y = y;
             Z = z;
         }
 
+        // Factory properties for creating common vectors
         public static Vector3 Zero { get { return new Vector3(0, 0, 0); } }
         public static Vector3 UnitX { get { return new Vector3(1, 0, 0); } }
         public static Vector3 UnitY { get { return new Vector3(0, 1, 0); } }
         public static Vector3 UnitZ { get { return new Vector3(0, 0, 1); } }
 
+        // unary negation
         public static Vector3 operator -(Vector3 v) {
-            return new Vector3(-v.X, -v.Y, -v.Z);
-        }
+            return new Vector3(-v.X, -v.Y, -v.Z);}
 
+        // Binary addition and subtraction operators
         public static Vector3 operator +(Vector3 a, Vector3 b) {
             return new Vector3(a.X+b.X, a.Y +b.Y, a.Z + b.Z);
         }
         public static Vector3 operator -(Vector3 a, Vector3 b) {
             return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
         }
+
+        // Scaling operators
         public static Vector3 operator *(Vector3 a, float f) {
             return new Vector3(a.X *f, a.Y *f, a.Z *f);
         }
@@ -72,18 +82,22 @@ namespace OneWeekendRT.Util {
             return a*f;
         }
 
+        // Lengths of the vector
         public float Length { get { return (float)Math.Sqrt(X * X + Y * Y + Z * Z); } }
         public float LengthSquared { get { return X * X + Y * Y + Z * Z; } }
 
-        public float Dot(Vector3 v) { return X * v.X + Y * v.Y + Z * v.Z; }
-
-        public Vector3 Cross(Vector3 v) {
-            return new Vector3(Y*v.Z - Z*v.Y, -(X*v.Z - Z*v.X), X*v.Y - Y*v.X);
-        }
-
+        // Normalize to a unit-vector
         public Vector3 Normalize() {
             var length = Length;
-            return new Vector3(X/length, Y/length, Z/length);
+            return new Vector3(X / length, Y / length, Z / length);
+        }
+
+        // Dot product
+        public float Dot(Vector3 v) { return X * v.X + Y * v.Y + Z * v.Z; }
+
+        // Cross product
+        public Vector3 Cross(Vector3 v) {
+            return new Vector3(Y*v.Z - Z*v.Y, -(X*v.Z - Z*v.X), X*v.Y - Y*v.X);
         }
     }
 }
