739983a39750f50574ee82ad0beaad57dd500305
ericrrichards <ericrrichards@gmail.com>
2016-04-10 06:19:25 -0400

    refactor some gunk

--- OneWeekendRT/Util/Ray.cs
+++ OneWeekendRT/Util/Ray.cs
@@ -1,15 +1,14 @@
 ﻿namespace OneWeekendRT.Util {
     public struct Ray {
-        public Vector3 A, B;
 
-        public Vector3 Origin {get { return A; }}
-        public Vector3 Direction { get { return B; } }
+        public Vector3 Origin { get; private set; }
+        public Vector3 Direction { get; private set; }
 
-        public Ray(Vector3 origin, Vector3 direction) {
-            A = origin;
-            B = direction;
+        public Ray(Vector3 origin, Vector3 direction) : this() {
+            Origin = origin;
+            Direction = direction;
         }
 
-        public Vector3 Evaluate(float t) { return A + B * t; }
+        public Vector3 Evaluate(float t) { return Origin + Direction * t; }
     }
 }
