66fb990c3f5dd69df5bbbefc6657027abaa20a26
Eric Richards <ericrrichards@gmail.com>
2018-09-15 11:05:02 -0400

    Start working on matrices

---
+++ RTC-Core-Tests/MatrixTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTC_Core_Tests {
+    using NUnit.Framework;
+
+    using RTC_Core;
+
+    public class MatrixTests {
+        [Test]
+        public void Construct4x4Matrix() {
+            var m = new Matrix(new[,] {
+                {1.0, 2, 3,4,},
+                {5.5, 6.5, 7.5, 8.5},
+                {9,10,11,12},
+                {13.5,14.5, 15.5, 16.5}
+            });
+            Assert.AreEqual(1, m[0, 0]);
+            Assert.AreEqual(4, m[0, 3]);
+            Assert.AreEqual(5.5, m[1, 0]);
+            Assert.AreEqual(7.5, m[1, 2]);
+            Assert.AreEqual(11, m[2, 2]);
+            Assert.AreEqual(13.5, m[3, 0]);
+            Assert.AreEqual(15.5, m[3, 2]);
+        }
+
+        [Test]
+        public void Construct2x2Matrix() {
+            var m = new Matrix(new[,] {
+                {-3.0, 5},
+                {1, -2}
+            });
+            Assert.AreEqual(-3, m[0, 0]);
+            Assert.AreEqual(5, m[0, 1]);
+            Assert.AreEqual(1, m[1, 0]);
+            Assert.AreEqual(-2, m[1, 1]);
+        }
+
+        [Test]
+        public void Construct3x3Matrix() {
+            var m = new Matrix(new[,] {
+                {-3, 5, 0.0},
+                {1, -2, -7},
+                {0,1,1}
+            });
+            Assert.AreEqual(-3, m[0, 0]);
+            Assert.AreEqual(-2, m[1, 1]);
+            Assert.AreEqual(1, m[2, 2]);
+        }
+
+        [Test]
+        public void MultiplyingMatrices() {
+            var a = new Matrix(new double[,] {
+                {1,2,3,4},
+                {2,3,4,5},
+                {3,4,5,6},
+                {4,5,6,7}
+            });
+            var b = new Matrix(new double[,] {
+                {0,1,2,4},
+                {1,2,4,8},
+                {2,4,8, 16},
+                {4, 8, 16, 32}
+            });
+            Assert.AreEqual(a*b, new Matrix(new double[,] {
+                {24, 49, 98, 196},
+                {31, 64, 128, 256},
+                {38, 79, 158, 316},
+                {45,94,188,376}
+            }));
+        }
+    }
+}
---
+++ RTC-Core/Matrix.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTC_Core {
+    public class Matrix : IEquatable<Matrix> {
+        private readonly double[,] _values;
+
+        public Matrix(double[,] v) {
+            _values = v;
+        }
+
+        public double this[int row, int column] { get => _values[row, column]; set => _values[row, column] = value; }
+        public int Rows => _values.GetLength(0);
+        public int Columns => _values.GetLength(1);
+        public static Matrix operator *(Matrix a, Matrix b) {
+            if (a.Rows != b.Rows || a.Columns != b.Columns) {
+                throw new InvalidOperationException("Matrix dimensions do not match");
+            }
+            var ret = new Matrix(new double[a.Rows,a.Columns]);
+            for (int row = 0; row < ret.Rows; row++) {
+                for (int col = 0; col < ret.Columns; col++) {
+                    ret[row, col] = a[row, 0] * b[0, col] +
+                                    a[row, 1] * b[1, col] + 
+                                    a[row, 2] * b[2, col] + 
+                                    a[row, 3] * b[3, col];
+                }
+            }
+            return ret;
+        }
+
+        public bool Equals(Matrix other) {
+            if (ReferenceEquals(null, other)) {
+                return false;
+            }
+            if (ReferenceEquals(this, other)) {
+                return true;
+            }
+            if (Rows != other.Rows || Columns != other.Columns) {
+                return false;
+            }
+            for (int row = 0; row < Rows; row++) {
+                for (int col = 0; col < Columns; col++) {
+                    if (this[row, col] != other[row, col]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) {
+                return false;
+            }
+            if (ReferenceEquals(this, obj)) {
+                return true;
+            }
+            if (obj.GetType() != this.GetType()) {
+                return false;
+            }
+            return Equals((Matrix)obj);
+        }
+
+        public override int GetHashCode() {
+            return (_values != null ? _values.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Matrix left, Matrix right) {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Matrix left, Matrix right) {
+            return !Equals(left, right);
+        }
+    }
+}
