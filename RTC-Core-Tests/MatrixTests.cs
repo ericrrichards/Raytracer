ddb9fb33497f1ce5dc4d4bc917dd24289018f047
Eric Richards <ericrrichards@gmail.com>
2018-09-17 18:36:55 -0400

    More matrix tests

--- RTC-Core-Tests/MatrixTests.cs
+++ RTC-Core-Tests/MatrixTests.cs
@@ -63,12 +63,257 @@ namespace RTC_Core_Tests {
                 {2,4,8, 16},
                 {4, 8, 16, 32}
             });
-            Assert.AreEqual(a*b, new Matrix(new double[,] {
+            Assert.AreEqual(a * b, new Matrix(new double[,] {
                 {24, 49, 98, 196},
                 {31, 64, 128, 256},
                 {38, 79, 158, 316},
                 {45,94,188,376}
             }));
+        }
+
+        [Test]
+        public void MultiplyMatrixWithTuple() {
+            var a = new Matrix(new double[,] {
+                {1,2,3,4},
+                {2,4,4,2},
+                {8,6,4,1},
+                {0,0,0,1}
+            });
+            var b = new Tuple(1, 2, 3, 1);
+            Assert.AreEqual(new Tuple(18, 24, 33, 1), a * b);
+        }
+
+        [Test]
+        public void MultiplyingByTheIdentityMatrix() {
+            var a = new Matrix(
+                               new double[,] {
+                                   { 0, 1, 2, 4 },
+                                   { 1, 2, 4, 8 },
+                                   { 2, 4, 8, 16 },
+                                   { 4, 8, 16, 32 }
+                               });
+            Assert.AreEqual(a, a*Matrix.Identity);
+        }
+
+        [Test]
+        public void MultipleTupleWithIdentityMatrix() {
+            var a = new Tuple(1,2,3,4);
+            Assert.AreEqual(a, Matrix.Identity*a);
+        }
+
+        [Test]
+        public void TransposeMatrix() {
+            var a = new Matrix(new double[,] {
+                {0,9,3,0},
+                {9,8,0,8},
+                {1,8,5,3},
+                {0,0,5,8}
+            });
+            var b = new Matrix(new double[,] {
+                {0,9,1,0},
+                {9,8,8,0},
+                {3,0,5,5},
+                {0,8,3,8}
+            });
+            Assert.AreEqual(b, a.Transpose);
+        }
+
+        [Test]
+        public void TransposeIdentityMatrix() {
+            Assert.AreEqual(Matrix.Identity, Matrix.Identity.Transpose);
+        }
+
+        [Test]
+        public void Determinant2x2Matrix() {
+            var a = new Matrix(new double[,] {
+                {1,5},
+                {-3,2}
+            });
+            Assert.AreEqual(17, a.Determinant);
+        }
+
+        [Test]
+        public void SubmatrixOf3x3MatrixIs2x2Matrix() {
+            var a = new Matrix(new double[,] {
+                {1,5,0},
+                {-3, 2,7},
+                {0, 6, -3}
+            });
+            var b = new Matrix(new double[,] {
+                {-3, 2},
+                {0,6}
+            });
+            Assert.AreEqual(b, a.SubMatrix(0,2));
+        }
+
+        [Test]
+        public void SubmatrixOf4x4MatrixIs3x3Matrix() {
+            var a = new Matrix(new double[,] {
+                {-6, 1,1,6},
+                {-8,5,8,6},
+                {-1,0,8,2},
+                {-7,1,-1,1}
+            });
+            var b = new Matrix(new double[,] {
+                {-6,1,6},
+                {-8,8,6},
+                {-7,-1,1}
+            });
+            Assert.AreEqual(b, a.SubMatrix(2,1));
+        }
+
+        [Test]
+        public void MinorOf3x3Matrix() {
+            var a = new Matrix(new double[,] {
+                {3,5,0},
+                {2,-1,-7},
+                {6,-1,5}
+            });
+            var b = a.SubMatrix(1, 0);
+            Assert.AreEqual(25, b.Determinant);
+            Assert.AreEqual(25, a.Minor(1,0));
+        }
+
+        [Test]
+        public void CofactorOf3x3Matrix() {
+            var a = new Matrix(
+                               new double[,] {
+                                   { 3, 5, 0 },
+                                   { 2, -1, -7 },
+                                   { 6, -1, 5 }
+                               });
+            Assert.AreEqual(-12, a.Minor(0,0));
+            Assert.AreEqual(-12, a.CoFactor(0,0));
+            Assert.AreEqual(25, a.Minor(1,0));
+            Assert.AreEqual(-25, a.CoFactor(1,0));
+        }
+
+        [Test]
+        public void DeterminantOf3x3Matrix() {
+            var a = new Matrix(new double[,] {
+                {1,2,6},
+                {-5,8,-4},
+                {2,6,4}
+            });
+            Assert.AreEqual(56, a.CoFactor(0,0));
+            Assert.AreEqual(12, a.CoFactor(0,1));
+            Assert.AreEqual(-46, a.CoFactor(0, 2));
+            Assert.AreEqual(-196, a.Determinant);
+        }
+
+        [Test]
+        public void DeterminantOf4x4Matrix() {
+            var a = new Matrix(new double[,] {
+                {-2, -8, 3, 5},
+                {-3,1,7,3},
+                {1,2,-9,6},
+                {-6,7,7,-9}
+            });
+            Assert.AreEqual(690, a.CoFactor(0,0));
+            Assert.AreEqual(447, a.CoFactor(0,1));
+            Assert.AreEqual(210, a.CoFactor(0,2));
+            Assert.AreEqual(51, a.CoFactor(0,3));
+            Assert.AreEqual(-4071, a.Determinant);
+        }
+
+        [Test]
+        public void Invertible() {
+            var a = new Matrix(new double[,] {
+                {6,4,4,4},
+                {5,5,7,6},
+                {4,-9,3,-7},
+                {9,1,7,-6}
+            });
+            Assert.AreEqual(-2120, a.Determinant);
+            Assert.True(a.Invertible);
+        }
+
+        [Test]
+        public void NotInvertible() {
+            var a = new Matrix(
+                               new double[,] {
+                                   { -4, 2, -2, -3 },
+                                   { 9, 6, 2, 6 },
+                                   { 0, -5, 1, -5 },
+                                   { 0, 0, 0, 0 }
+                               });
+            Assert.AreEqual(0, a.Determinant);
+            Assert.False(a.Invertible);
+        }
+
+        [Test]
+        public void Inverse() {
+            var a = new Matrix(new double[,] {
+                {-5,2,6,-8},
+                {1,-5,1,8},
+                {7,7,-6,-7},
+                {1,-3,7,4}
+            });
+            var b = a.Inverse();
+            Assert.AreEqual(532, a.Determinant);
+            Assert.AreEqual(-160, a.CoFactor(2,3));
+            Assert.AreEqual(-160.0/532, b[3,2]);
+            Assert.AreEqual(105, a.CoFactor(3,2));
+            Assert.AreEqual(105.0/532, b[2,3]);
+            var expected = new Matrix(new double[,] {
+                {0.21805, 0.45113, 0.24060, -0.04511},
+                {-0.80827, -1.45677, -0.44361, 0.52068},
+                {-0.07895, -0.22368, -0.05263, 0.19737},
+                {-0.52256, -0.81391, -0.30075, 0.30639}
+            });
+            Assert.AreEqual(expected, b);
+        }
+
+        [Test]
+        public void Inverse2() {
+            var a = new Matrix(new double[,] {
+                {8, -5, 9, 2},
+                {7,5,6,1},
+                {-6,0,9,6},
+                {-3,0,-9,-4}
+            });
+            var b = new Matrix(new double[,] {
+                {-0.15385, -0.15385, -0.28205, -0.53846},
+                {-0.07692, 0.12308, 0.02564, 0.03077},
+                {0.35897, 0.35897, 0.43590, 0.92308},
+                {-0.69231, -0.69231, -0.76923, -1.92308}
+            });
+            Assert.AreEqual(b, a.Inverse());
+        }
+        [Test]
+        public void Inverse3() {
+            var a = new Matrix(new double[,] {
+                {9, 3, 0, 9},
+                {-5,-2,-6,-3},
+                {-4,9,6,4},
+                {-7,6,6,2}
+            });
+            var b = new Matrix(new double[,] {
+                {-0.04074, -0.07778, 0.14444, -0.22222},
+                {-0.07778, 0.03333, 0.36667, -0.33333},
+                {-0.02901, -0.14630, -0.10926, 0.12963},
+                {0.17778, 0.06667, -0.26667, 0.33333}
+            });
+            Assert.AreEqual(b, a.Inverse());
+        }
+
+        [Test]
+        public void MultipleProductByInverse() {
+            var a = new Matrix(
+                               new double[,] {
+                                   { 3, -9, 7, 3 },
+                                   { 3, -8, 2, -9 },
+                                   { -4, 4, 4, 1 },
+                                   { -6, 5, -1, 1 }
+                               });
+            var b = new Matrix(new double[,] {
+                {8,2,2,2},
+                {3,-1,7,0},
+                {7,0,5,4},
+                {6, -2, 0, 5}
+            });
+            var c = a * b;
+            Assert.AreEqual(a, c*b.Inverse());
         }
     }
 }
--- RTC-Core/Matrix.cs
+++ RTC-Core/Matrix.cs
@@ -6,8 +6,20 @@ namespace RTC_Core {
     public class Matrix : IEquatable<Matrix> {
         private readonly double[,] _values;
 
+        public static Matrix Identity => new Matrix(new double[,] {
+            {1,0,0,0},
+            {0,1,0,0},
+            {0,0,1,0},
+            {0,0,0,1}
+        });
+
+
         public Matrix(double[,] v) {
             _values = v;
+        }
+
+        private Matrix(int rows, int cols) {
+            _values = new double[rows,cols];
         }
 
         public double this[int row, int column] { get => _values[row, column]; set => _values[row, column] = value; }
@@ -17,9 +29,9 @@ namespace RTC_Core {
             if (a.Rows != b.Rows || a.Columns != b.Columns) {
                 throw new InvalidOperationException("Matrix dimensions do not match");
             }
-            var ret = new Matrix(new double[a.Rows,a.Columns]);
-            for (int row = 0; row < ret.Rows; row++) {
-                for (int col = 0; col < ret.Columns; col++) {
+            var ret = new Matrix(a.Rows,a.Columns);
+            for (var row = 0; row < ret.Rows; row++) {
+                for (var col = 0; col < ret.Columns; col++) {
                     ret[row, col] = a[row, 0] * b[0, col] +
                                     a[row, 1] * b[1, col] + 
                                     a[row, 2] * b[2, col] + 
@@ -29,6 +41,95 @@ namespace RTC_Core {
             return ret;
         }
 
+        public static Tuple operator *(Matrix a, Tuple b) {
+            if (a.Rows != 4 || a.Columns != 4) {
+                throw new InvalidOperationException("Not 4x4 matrix");
+            }
+            return new Tuple(
+                             a[0,0]*b.X + a[0,1]*b.Y + a[0,2]*b.Z + a[0,3]*b.W,
+                             a[1,0]*b.X + a[1,1]*b.Y + a[1,2]*b.Z + a[1,3]*b.W,
+                             a[2,0]*b.X + a[2,1]*b.Y + a[2,2]*b.Z + a[2,3]*b.W,
+                             a[3,0]*b.X + a[3,1]*b.Y + a[3,2]*b.Z + a[3,3]*b.W
+                             );
+        }
+
+        public Matrix Transpose {
+            get {
+                var ret = new Matrix(Rows, Columns);
+                for (var row = 0; row < Rows; row++) {
+                    for (var col = 0; col < Columns; col++) {
+                        ret[col, row] = _values[row, col];
+                    }
+                }
+                return ret;
+            }
+        }
+        public double Determinant {
+            get {
+                if (Rows == 2 && Columns == 2) {
+                    return this[0, 0] * this[1, 1] - this[0, 1] * this[1, 0];
+                }
+                if (Rows == 3 && Columns == 3) {
+                    return this[0, 0] * CoFactor(0, 0) + this[0, 1] * CoFactor(0, 1) + this[0, 2] * CoFactor(0, 2);
+                }
+                if (Rows == 4 && Columns == 4) {
+                    return this[0, 0] * CoFactor(0, 0) + this[0, 1] * CoFactor(0, 1) + this[0, 2] * CoFactor(0, 2) + this[0,3]*CoFactor(0,3);
+                }
+                throw new NotImplementedException();
+            }
+        }
+        public bool Invertible => Determinant != 0;
+
+        public Matrix Inverse() {
+            var ret = new Matrix(Rows, Columns);
+            for (var row = 0; row < Rows; row++) {
+                for (var col = 0; col < Columns; col++) {
+                    ret[row, col] = CoFactor(row, col);
+                }
+            }
+            ret = ret.Transpose;
+            var determinant = Determinant;
+            for (var row = 0; row < Rows; row++) {
+                for (var col = 0; col < Columns; col++) {
+                    
+                    ret[row, col] = ret[row,col] / determinant;
+                }
+            }
+            return ret;
+        }
+
+        public Matrix SubMatrix(int row, int col) {
+            var ret = new Matrix(Rows-1, Columns-1);
+            
+            var r1 = 0;
+            for (var r = 0; r < Rows; r++) {
+                var c1 = 0;
+                if (r == row) {
+                    continue;
+                }
+                for (var c = 0; c < Columns; c++) {
+                    if (c == col) {
+                        continue;
+                    }
+                    ret[r1, c1] = _values[r, c];
+                    c1++;
+                }
+                r1++;
+            }
+            return ret;
+        }
+
+        public double Minor(int row, int col) => SubMatrix(row, col).Determinant;
+        public double CoFactor(int row, int col) {
+            if ((row + col) % 2 == 0) {
+                return Minor(row, col);
+            }
+            return -Minor(row, col);
+        }
+
+
+
+        #region Equality stuff
         public bool Equals(Matrix other) {
             if (ReferenceEquals(null, other)) {
                 return false;
@@ -39,9 +140,9 @@ namespace RTC_Core {
             if (Rows != other.Rows || Columns != other.Columns) {
                 return false;
             }
-            for (int row = 0; row < Rows; row++) {
-                for (int col = 0; col < Columns; col++) {
-                    if (this[row, col] != other[row, col]) {
+            for (var row = 0; row < Rows; row++) {
+                for (var col = 0; col < Columns; col++) {
+                    if (!NumericExtensions.Equals(this[row, col], other[row, col], 0.00001)){
                         return false;
                     }
                 }
@@ -72,6 +173,18 @@ namespace RTC_Core {
 
         public static bool operator !=(Matrix left, Matrix right) {
             return !Equals(left, right);
+        }
+        #endregion
+
+        public override string ToString() {
+            var sb = new StringBuilder();
+            for (var row = 0; row < Rows; row++) {
+                for (var col = 0; col < Columns; col++) {
+                    sb.Append(this[row, col] + ",");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
         }
     }
 }
